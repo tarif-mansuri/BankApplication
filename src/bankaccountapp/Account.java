c2341d0a48f6289c4d2bfb7416593a014b026435
tarif.m <tarif.m@zohocorp.com>
2019-04-12 09:48:16 +0530

    Adding COnstructors

--- src/bankaccountapp/Account.java
+++ src/bankaccountapp/Account.java
@@ -2,9 +2,17 @@ package bankaccountapp;
 
 public abstract class Account implements IBaseRate {
 	//List common properties for saving and checking accounts
+	private String name;
+	private String accountNumber;
+	private double balance;
+	private double rate;
+	private String sSN; // Social Security number
+	
 	//Constructor to set base properties and initialize the account
-	public Account() {
-		System.out.print("New Account:");
+	public Account(String name, String sSN, int initDeposit) {
+		this.name = name;
+		this.sSN = sSN;
+		this.balance = initDeposit;
 	}
 	//List of common methods
 }
--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -2,6 +2,8 @@ package bankaccountapp;
 
 public class BankAccountApp {
 	public static void main(String[] args) {
-		Checking ch1 = new Checking();
+		Checking ch1 = new Checking("Tarif Mansuri","759327495",1500);
+		
+		Savings saving1 = new Savings("Vasanthi Kumari","47382634",1000);
 	}
 }
