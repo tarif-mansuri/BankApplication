97a604b15e5f494512097810118d0c9d35c7279b
tarif.m <tarif.m@zohocorp.com>
2019-04-13 00:15:30 +0530

    Adding csv reader support

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,12 +1,17 @@
 package bankaccountapp;
 
+import java.util.List;
+
+import utilities.CSV;
+
 public class BankAccountApp {
 	public static void main(String[] args) {
-		Checking ch1 = new Checking("Tarif Mansuri","759327495",1500);
-		
-		Savings saving1 = new Savings("Vasanthi Kumari","47382634",1000);
-		ch1.showInfo();
-		System.out.println("***************");
-		saving1.showInfo();
+		List<String[]> csvData = CSV.read("/home/local/ZOHOCORP/tarif-5329/Documents/workspace-spring-tool-suite-4-4.2.0.RELEASE/BankApplication/lib/NewBankAccounts.csv");
+		for(String[] accountHolder : csvData) {
+			System.out.println(accountHolder[0]);
+			System.out.println(accountHolder[1]);
+			System.out.println(accountHolder[2]);
+			System.out.println(accountHolder[3]);
+		}
 	}
 }
